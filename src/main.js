2f3389270edfc888c9db87d681ea46703986bdac
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-04-26 16:13:17 +0300

    radio and selector fix

--- src/main.js
+++ src/main.js
@@ -2,15 +2,32 @@ const selectedThemeByRadio = document.querySelectorAll('input[name="theme"]');
 const links = document.querySelectorAll('.link');
 const buttons = document.querySelectorAll('.button');
 const selector = document.getElementById('select');
+
 const headerElement = document.getElementById('header');
 const sectionElement = document.getElementById('section');
 const footerElement = document.getElementById('footer');
+
+const lightThemeButton = document.getElementById('lightTheme');
+const darkThemeButton = document.getElementById('darkTheme');
+const greyThemeButton = document.getElementById('greyTheme');
+
+const lightThemeSelect = document.getElementById('lightSelect');
+const darkThemeSelect = document.getElementById('darkSelect');
+const greyThemeSelect = document.getElementById('greySelect');
 
 function changeTheme(value) {
   if (value === 'light') {
     headerElement.className = 'header header-light';
     sectionElement.className = 'section section-light';
     footerElement.className = 'footer footer-light';
+
+    darkThemeButton.removeAttribute('checked');
+    greyThemeButton.removeAttribute('checked');
+    lightThemeButton.setAttribute('checked', true);
+
+    greyThemeSelect.removeAttribute('selected');
+    darkThemeSelect.removeAttribute('selected');
+    lightThemeSelect.setAttribute('selected', true);
 
     links.forEach((link) => {
       link.classList.remove('link-light');
@@ -23,6 +40,14 @@ function changeTheme(value) {
     sectionElement.className = 'section section-dark';
     footerElement.className = 'footer footer-dark';
 
+    lightThemeButton.removeAttribute('checked');
+    greyThemeButton.removeAttribute('checked');
+    darkThemeButton.setAttribute('checked', true);
+
+    greyThemeSelect.removeAttribute('selected');
+    lightThemeSelect.removeAttribute('selected');
+    darkThemeSelect.setAttribute('selected', true);
+
     links.forEach((link) => {
       link.classList.remove('link');
       link.classList.add('link-light');
@@ -33,6 +58,14 @@ function changeTheme(value) {
     headerElement.className = 'header header-grey';
     sectionElement.className = 'section section-grey';
     footerElement.className = 'footer footer-grey';
+
+    lightThemeButton.removeAttribute('checked');
+    darkThemeButton.removeAttribute('checked');
+    greyThemeButton.setAttribute('checked', true);
+
+    lightThemeSelect.removeAttribute('selected');
+    darkThemeSelect.removeAttribute('selected');
+    greyThemeSelect.setAttribute('selected', true);
 
     links.forEach((link) => {
       link.classList.remove('link');
